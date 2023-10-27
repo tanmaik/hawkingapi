35b720a71c27d54c3ad62e4ed6501e4991bb54a5
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2023-10-26 20:14:50 -0400

    Add cors import ;'(

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const HttpError = require("./models/http-error");
+const cors = require("cors");
 
 require("dotenv").config();
 
